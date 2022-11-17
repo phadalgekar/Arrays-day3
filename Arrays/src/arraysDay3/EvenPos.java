d6f6c1504de69c105e39d1839ccb9334b7463992
phadalgekar <116626533+phadalgekar@users.noreply.github.com>
2022-11-17 12:59:30 +0530

    Add files via upload

---
+++ Arrays/src/arraysDay3/EvenPos.java
@@ -0,0 +1,17 @@
+package arraysDay3;
+
+public class EvenPos {
+	public static void main(String args[]) {
+	    int [] numbers = new int [] {5, 12, 16, 3, 9, 7, 1, 100};
+	    System.out.println("Array Elements on even Positions: ");
+	    for (int i = 1; i < numbers.length; i = i+2) {
+	        System.out.println(numbers[i]);
+
+	    }
+
+
+	    
+
+	}
+
+}
---
+++ Arrays/src/arraysDay3/OddPos.java
@@ -0,0 +1,19 @@
+package arraysDay3;
+
+public class OddPos {
+	public static void main(String args[]) {
+	    int [] numbers = new int [] {5, 12, 16, 3, 9, 7, 1, 100};
+	    System.out.println("Array Elements on odd Positions: ");
+	    for (int i = 0; i < numbers.length; i = i+2) {
+	        System.out.println(numbers[i]);
+
+	    }
+
+
+	    
+
+	}
+
+
+	
+}
